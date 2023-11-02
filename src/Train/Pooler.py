4a83a73099d10e996bd4c402f7ad9a26ad10af06
bee85919 <bee85919@gmail.com>
2023-11-03 01:38:25 +0900

    Refactoring # 8
    - Pooler.py

--- src/Train/Pooler.py
+++ src/Train/Pooler.py
@@ -1,20 +1,9 @@
 from multiprocessing import Pool
 from .Train import Train
 
-
 class Pooler:
-    def __init__(self, symbols, p_num=4):
-        self.p_num = p_num
-        self.symbols = symbols
-        self.len_symbols = len(symbols)
-        self.execute()
-
-
-    def process_and_count(self, args):
-        idx, symbol = args
-        Train.train_and_save(symbol, idx=idx, len_symbols=self.len_symbols)
-
-
-    def execute(self):
-        with Pool(processes=self.p_num) as p:
-            p.map(self.process_and_count, enumerate(self.symbols))
+    def __init__(self, syms, pnum=8):
+        size = len(syms)
+        task = [(i, sym, size) for i, sym in enumerate(syms, start=1)]
+        with Pool(processes=pnum) as p:
+            p.starmap(Train, task)
