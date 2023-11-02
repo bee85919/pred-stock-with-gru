0cc63500476a3f1b2db56772a706fbf17416880e
bee85919 <bee85919@gmail.com>
2023-11-03 01:38:11 +0900

    Refactoring # 7
    - Train.py

--- src/Train/Train.py
+++ src/Train/Train.py
@@ -1,52 +1,42 @@
 import os
 import pandas as pd
 from .Normalizer import Normalizer
-from .GRUTrainer import GRUTrainer
-from .Logger import Logger
+from .GRUModeling import GRUModeling
 from Utils.envLoader import envLoader
+from Utils.Logger import Logger
 
-get_path = envLoader().get_path
+
+pth = envLoader().path.get
+log = Logger.log
+err = Logger.err
 
 
-class Train:
+class Train:        
     
-    @staticmethod
-    def make_dir():
-        os.makedirs(get_path('logs_path'), exist_ok=True)
-        os.makedirs(get_path('pred_path'), exist_ok=True)
-        
+    def __init__(self, sym, i, size):
+        logf = f"{pth('logs')}/l{sym}.txt"  
+        X, y = self.read(sym)
+        if not X or not y: 
+            err(logf, sym)
+            return
+        X_train, y_train, X_test, sc = Normalizer(X, y, 5, 2)
+        pred = GRUModeling(X_train, y_train, X_test, sc, sym, i, size)
+        self.save(sym, pred, logf, sym, X_train, X_test, y_train, sc, pred, y)
 
-    @staticmethod
-    def read_Xy(symbol):
-        X_path = os.path.join(get_path('train_path'), f"train_{symbol}.csv")
-        y_path = os.path.join(get_path('test_path'), f"test_{symbol}.csv")
-        cond = os.path.exists(X_path) and os.path.exists(y_path)
-        if cond: return pd.read_csv(X_path), pd.read_csv(y_path)
+
+    def read(sym):
+        X_path = os.path.join(pth('train'), f"train_{sym}.csv")
+        y_path = os.path.join(pth('test'), f"test_{sym}.csv")
+        if os.path.exists(X_path) and os.path.exists(y_path): 
+            return pd.read_csv(X_path), pd.read_csv(y_path)
         else: return None, None
-
-
-    @staticmethod
-    def create_and_save_df(pred, y, symbol):
-        pred_df = pd.DataFrame({'Adj Close': pred[:min(len(pred), len(y)-1), 0]})
-        pred_df.to_csv(os.path.join(get_path('pred_path'), f"pred_{symbol}.csv"), index=False)
-        return pred_df
-
-
-    @staticmethod
-    def train_and_save(symbol, idx=0, len_symbols=0):
-        try:
-            log = f"{get_path('logs_path')}/log_{symbol}.txt"
-            gru = GRUTrainer(idx=idx, len_symbols=len_symbols)
-            gru.initialize_model()
-            
-            X, y = Train.read_Xy(symbol)
-            if X is not None and y is not None:
-                X_train, y_train, X_test, sc = Normalizer(X, y, time_steps=5, for_periods=2).normalize()
-                pred    = gru.train(X_train, y_train, X_test, sc, symbol)
-                pred_df = Train.create_and_save_df(pred, y, symbol)
-                Logger.log(log, symbol, X_train, X_test, y_train, sc, pred_df)
-                
-            else:
-                Logger.err(log, symbol)
-        except Exception as e:
-            print(f"An error occurred: {e}")
+    
+    
+    def adjust(self, pred, y):
+        return pd.DataFrame({'Adj Close': pred[:min(len(pred), len(y)-1), 0]})
+        
+        
+    def save(self, logf, sym, X_train, X_test, y_train, sc, pred, y):        
+        pred = self.adjust(pred, y)
+        pred.to_csv(os.path.join(pth('pred'), f"pred_{sym}.csv"), index=False)
+        log(logf, sym, X_train, X_test, y_train, sc, pred)
